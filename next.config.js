2496d9972b0159f345a9af526f6720297dcc1b7e
jonybur <jobur93@gmail.com>
2023-06-20 22:04:36 +0000

    First working version

--- next.config.js
+++ next.config.js
@@ -1,4 +1,28 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
-module.exports = nextConfig
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.plugins.push(
+        new CopyWebpackPlugin({
+          patterns: [
+            {
+              from: path.resolve(
+                __dirname,
+                "node_modules/@aztec/bb.js/dest/barretenberg.wasm"
+              ),
+              to: path.resolve(__dirname, "public/barretenberg.wasm"),
+            },
+          ],
+        })
+      );
+    }
+
+    console.log(
+      "CopyWebpackPlugin executed! Check /public for barretenberg.wasm"
+    );
+
+    return config;
+  },
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,99 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+
+import Image from "next/image";
+import styles from "./page.module.css";
+// for some reason, importing newBarretenbergApiSync doesnt work
+import { BarretenbergBinderSync } from "@aztec/bb.js/dest/barretenberg_binder";
+import { BarretenbergApiSync } from "@aztec/bb.js/dest/barretenberg_api";
+import * as bbjs from "@aztec/bb.js/dest/barretenberg_wasm";
+import { useEffect, useState } from "react";
+import { Buffer32, Fr } from "@aztec/bb.js/dest/types";
+import { BarretenbergApiAsync } from "@aztec/bb.js";
+
+const DEMO_STRING =
+  "abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789";
+
+function blake2sSync(api: BarretenbergApiSync) {
+  const input = Buffer.from(DEMO_STRING);
+  const expected = Buffer32.fromBuffer(
+    new Uint8Array([
+      0x44, 0xdd, 0xdb, 0x39, 0xbd, 0xb2, 0xaf, 0x80, 0xc1, 0x47, 0x89, 0x4c,
+      0x1d, 0x75, 0x6a, 0xda, 0x3d, 0x1c, 0x2a, 0xc2, 0xb1, 0x00, 0x54, 0x1e,
+      0x04, 0xfe, 0x87, 0xb4, 0xa5, 0x9e, 0x12, 0x43,
+    ])
+  );
+  const result = api.blake2s(input);
+  return { result, expected };
+}
+
+function blake2FieldSync(api: BarretenbergApiSync) {
+  const input = Buffer.from(DEMO_STRING);
+  const expected = Fr.fromBufferReduce(
+    new Uint8Array([
+      0x44, 0xdd, 0xdb, 0x39, 0xbd, 0xb2, 0xaf, 0x80, 0xc1, 0x47, 0x89, 0x4c,
+      0x1d, 0x75, 0x6a, 0xda, 0x3d, 0x1c, 0x2a, 0xc2, 0xb1, 0x00, 0x54, 0x1e,
+      0x04, 0xfe, 0x87, 0xb4, 0xa5, 0x9e, 0x12, 0x43,
+    ])
+  );
+  const result = api.blake2sToField(input);
+  return { result, expected };
+}
+
+// async function blake2sAsync(api: BarretenbergApiAsync) {
+//   const input = Buffer.from(DEMO_STRING);
+//   const expected = Buffer32.fromBuffer(
+//     new Uint8Array([
+//       0x44, 0xdd, 0xdb, 0x39, 0xbd, 0xb2, 0xaf, 0x80, 0xc1, 0x47, 0x89, 0x4c,
+//       0x1d, 0x75, 0x6a, 0xda, 0x3d, 0x1c, 0x2a, 0xc2, 0xb1, 0x00, 0x54, 0x1e,
+//       0x04, 0xfe, 0x87, 0xb4, 0xa5, 0x9e, 0x12, 0x43,
+//     ])
+//   );
+//   const result = await api.blake2s(input);
+//   return { result, expected };
+// }
+
+// async function blake2FieldAsync(api: BarretenbergApiAsync) {
+//   const input = Buffer.from(DEMO_STRING);
+//   const expected = Fr.fromBufferReduce(
+//     new Uint8Array([
+//       0x44, 0xdd, 0xdb, 0x39, 0xbd, 0xb2, 0xaf, 0x80, 0xc1, 0x47, 0x89, 0x4c,
+//       0x1d, 0x75, 0x6a, 0xda, 0x3d, 0x1c, 0x2a, 0xc2, 0xb1, 0x00, 0x54, 0x1e,
+//       0x04, 0xfe, 0x87, 0xb4, 0xa5, 0x9e, 0x12, 0x43,
+//     ])
+//   );
+//   const result = await api.blake2sToField(input);
+//   return { result, expected };
+// }
 
 export default function Home() {
+  const [result, setResult] = useState<{
+    result: Buffer32;
+    expected: Buffer32;
+  }>();
+
+  const [fieldResult, setFieldResult] = useState<{
+    result: Fr;
+    expected: Fr;
+  }>();
+
+  useEffect(() => {
+    (async function () {
+      // for sync:
+      const bbWasm = await bbjs.BarretenbergWasm.new();
+      const bbBinderSync = new BarretenbergBinderSync(bbWasm);
+      const bbApi = new BarretenbergApiSync(bbBinderSync);
+      setResult(blake2sSync(bbApi));
+      setFieldResult(blake2FieldSync(bbApi));
+
+      // for async:
+      // const threads = 1;
+      // const { wasm, worker } = await bbjs.BarretenbergWasm.newWorker(threads);
+      // const bbApi = new BarretenbergApiAsync(worker, wasm);
+      // setResult(await blake2sAsync(bbApi));
+      // setFieldResult(await blake2FieldAsync(bbApi));
+    })();
+  }, []);
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -15,7 +107,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -29,16 +121,29 @@ export default function Home() {
       </div>
 
       <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+        {`blake2s ${DEMO_STRING}:`}
+        <br />
+        <br />
+        {result ? (
+          <div>
+            blake2s: {result.result.buffer}
+            <br />
+            expected: {result.expected.buffer}
+          </div>
+        ) : (
+          "Loading..."
+        )}
+        <br />
+        {fieldResult ? (
+          <div>
+            blake2sToField: {fieldResult.result.toString()}
+            <br />
+            expected: {fieldResult.expected.toString()}
+          </div>
+        ) : (
+          "Loading..."
+        )}
       </div>
-
       <div className={styles.grid}>
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
@@ -91,5 +196,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
