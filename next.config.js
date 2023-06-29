c3036b2e6e47f37b27eacc7a97230238dfea8327
jonybur <jobur93@gmail.com>
2023-06-29 09:45:19 +0000

    Cleanup

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    esmExternals: true,
+  },
+};
+
+module.exports = nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,40 +2,51 @@
 
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import { proxy } from "comlink";
+import {
+  createWorker,
+  getRemoteBarretenbergWasm,
+} from "@aztec/bb.js/dest/browser/barretenberg_wasm/browser/index";
+
+import * as bbjs from "@aztec/bb.js/dest/browser";
+import { BarretenbergApiAsync } from "@aztec/bb.js/dest/browser/factory";
+import { Buffer32, Fr } from "@aztec/bb.js/dest/browser/types";
+import { BarretenbergApiSync } from "@aztec/bb.js/dest/browser/barretenberg_api";
+import { BarretenbergBinderSync } from "@aztec/bb.js/dest/browser/barretenberg_binder";
+
 import styles from "./page.module.css";
 
-// need to fix the imports
-import * as bbjs from "@aztec/bb.js/dest/browser/barretenberg_wasm";
-import { BarretenbergBinderSync } from "@aztec/bb.js/dest/browser/barretenberg_binder";
-import { BarretenbergApiSync } from "@aztec/bb.js/dest/browser/barretenberg_api";
-import { Buffer32, Fr } from "@aztec/bb.js/dest/browser/types";
+const DEMO_INPUT = Buffer.from(
+  "abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789"
+);
 
-const DEMO_STRING =
-  "abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789";
+const TEST_BUFFER = new Uint8Array([
+  0x44, 0xdd, 0xdb, 0x39, 0xbd, 0xb2, 0xaf, 0x80, 0xc1, 0x47, 0x89, 0x4c, 0x1d,
+  0x75, 0x6a, 0xda, 0x3d, 0x1c, 0x2a, 0xc2, 0xb1, 0x00, 0x54, 0x1e, 0x04, 0xfe,
+  0x87, 0xb4, 0xa5, 0x9e, 0x12, 0x43,
+]);
 
 function blake2sSync(api: BarretenbergApiSync) {
-  const input = Buffer.from(DEMO_STRING);
-  const expected = Buffer32.fromBuffer(
-    new Uint8Array([
-      0x44, 0xdd, 0xdb, 0x39, 0xbd, 0xb2, 0xaf, 0x80, 0xc1, 0x47, 0x89, 0x4c,
-      0x1d, 0x75, 0x6a, 0xda, 0x3d, 0x1c, 0x2a, 0xc2, 0xb1, 0x00, 0x54, 0x1e,
-      0x04, 0xfe, 0x87, 0xb4, 0xa5, 0x9e, 0x12, 0x43,
-    ])
-  );
-  const result = api.blake2s(input);
+  const expected = Buffer32.fromBuffer(TEST_BUFFER);
+  const result = api.blake2s(DEMO_INPUT);
+  return { result, expected };
+}
+
+async function blake2sAsync(api: BarretenbergApiAsync) {
+  const expected = Buffer32.fromBuffer(TEST_BUFFER);
+  const result = await api.blake2s(DEMO_INPUT);
   return { result, expected };
 }
 
 function blake2FieldSync(api: BarretenbergApiSync) {
-  const input = Buffer.from(DEMO_STRING);
-  const expected = Fr.fromBufferReduce(
-    new Uint8Array([
-      0x44, 0xdd, 0xdb, 0x39, 0xbd, 0xb2, 0xaf, 0x80, 0xc1, 0x47, 0x89, 0x4c,
-      0x1d, 0x75, 0x6a, 0xda, 0x3d, 0x1c, 0x2a, 0xc2, 0xb1, 0x00, 0x54, 0x1e,
-      0x04, 0xfe, 0x87, 0xb4, 0xa5, 0x9e, 0x12, 0x43,
-    ])
-  );
-  const result = api.blake2sToField(input);
+  const expected = Fr.fromBufferReduce(TEST_BUFFER);
+  const result = api.blake2sToField(DEMO_INPUT);
+  return { result, expected };
+}
+
+async function blake2FieldAsync(api: BarretenbergApiAsync) {
+  const expected = Fr.fromBufferReduce(TEST_BUFFER);
+  const result = await api.blake2sToField(DEMO_INPUT);
   return { result, expected };
 }
 
@@ -51,12 +62,30 @@ export default function Home() {
   }>();
 
   useEffect(() => {
-    (async function () {
+    const asyncRun = async () => {
+      const NUM_THREADS = 2;
+      const worker = createWorker();
+      const wasm = getRemoteBarretenbergWasm(worker);
+      await wasm.init(
+        NUM_THREADS,
+        proxy(() => {})
+      );
+      const api = new BarretenbergApiAsync(worker, wasm);
+      setResult(await blake2sAsync(api));
+      setFieldResult(await blake2FieldAsync(api));
+    };
+
+    const syncRun = async () => {
       const bbWasm = await bbjs.BarretenbergWasm.new();
       const bbBinderSync = new BarretenbergBinderSync(bbWasm);
       const bbApi = new BarretenbergApiSync(bbBinderSync);
       setResult(blake2sSync(bbApi));
       setFieldResult(blake2FieldSync(bbApi));
+    };
+
+    (async function () {
+      // asyncRun();
+      syncRun();
     })();
   }, []);
 
@@ -87,9 +116,6 @@ export default function Home() {
       </div>
 
       <div className={styles.center}>
-        {`blake2s ${DEMO_STRING}:`}
-        <br />
-        <br />
         {result ? (
           <div>
             blake2s: {result.result.buffer}
