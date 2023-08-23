596781406b4b6a667639ce1c6d4f0ac3191f2424
Jonathan Bursztyn <jonathan@bursztyn.io>
2023-08-23 02:31:18 +0100

    Add console logs

--- src/tests/multithreading.test.js
+++ src/tests/multithreading.test.js
@@ -3,14 +3,30 @@ const { chromium } = require("playwright");
 async function multithreadingTest() {
   try {
     console.log("Running Multithreading test...");
+
+    console.log("Launching chromium browser...");
     const browser = await chromium.launch({ headless: false });
+
+    console.log("Opening a new page...");
     const page = await browser.newPage();
+
+    console.log("Navigating to http://localhost:3000/multithreading...");
     await page.goto("http://localhost:3000/multithreading");
+
+    console.log("Waiting for [data-test-id='blake2s-success'] selector...");
     await page.waitForSelector(`[data-test-id='blake2s-success']`);
+
+    console.log(
+      "Waiting for [data-test-id='blake2sToField-success'] selector..."
+    );
     await page.waitForSelector(`[data-test-id='blake2sToField-success']`);
+
+    console.log("Closing the browser...");
     await browser.close();
+
     console.log("Multithreading test succeeded");
   } catch {
+    console.error("An error occurred during the Multithreading test.");
     throw new Error("Multithreading test failed");
   }
 }
--- src/tests/recursion.test.js
+++ src/tests/recursion.test.js
@@ -3,19 +3,43 @@ const { chromium } = require("playwright");
 async function recursionTest() {
   try {
     console.log("Running Recursion test...");
+
+    console.log("Launching chromium browser...");
     const browser = await chromium.launch({ headless: true });
+
+    console.log("Opening a new page...");
     const page = await browser.newPage();
+
+    console.log("Navigating to http://localhost:3000/recursion...");
     await page.goto("http://localhost:3000/recursion");
+
+    console.log("Waiting for `input[value='init api']` selector...");
     await page.waitForSelector(`input[value='init api']`);
+
+    console.log("Clicking on `input[value='init api']` button...");
     await page.click(`input[value='init api']`);
+
+    console.log("Waiting for `input[value='prove']` selector...");
     await page.waitForSelector(`input[value='prove']`);
+
+    console.log("Clicking on `input[value='prove']` button...");
     await page.click(`input[value='prove']`);
+
+    console.log("Waiting for `input[value='proved']` selector...");
     await page.waitForSelector(`input[value='proved']`);
+
+    console.log("Clicking on `input[value='verify']` button...");
     await page.click(`input[value='verify']`);
+
+    console.log("Waiting for `input[value='verified']` selector...");
     await page.waitForSelector(`input[value='verified']`);
+
+    console.log("Closing the browser...");
     await browser.close();
+
     console.log("Recursion test succeeded");
   } catch {
+    console.error("An error occurred during the Recursion test.");
     throw new Error("Recursion test failed");
   }
 }
